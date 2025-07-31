338add3b601add2b7bc53c12707be641dce77b4a
Krishal <krishalbatra18@gmail.com>
2025-07-31 14:52:05 +0530

    first commit

---
+++ src/components/Cards/FaqCard.jsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React, { useState } from "react";
+import { FaPlus, FaMinus } from "react-icons/fa";
+import { MdKeyboardArrowRight, MdKeyboardArrowDown } from "react-icons/md";
+import { motion, AnimatePresence } from "framer-motion";
+
+const FaqCard = ({ question, answer }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="bg-[#171A86]/20 rounded-2xl m-5 p-4 mx-auto border-gray-300 w-[90%] ">
+      {/* Question Row clicking over any part of this element change the isOpen state ---> if open it closes and if closed it opens*/}
+      <div
+        className="flex justify-between items-center cursor-pointer"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        <div className="flex gap-2 items-center">
+          <span className="text-white">
+            {/* arrow icon change depending whether it the card is open or closed */}
+            {isOpen ? <MdKeyboardArrowDown size={24} /> : <MdKeyboardArrowRight size={24} />}
+          </span>
+          <h3 className="text-lg font-medium text-white">{question}</h3>
+        </div>
+        <span className="text-white px-1">
+          {/* plus/minus icon change depending whether it the card is open or closed */}
+          {isOpen ? <FaMinus size={18} /> : <FaPlus size={18} />}
+        </span>
+      </div>
+
+      {/* Animated Answer - the card expands when it is clicked */}
+      <AnimatePresence initial={false}>
+        {isOpen && (
+          <motion.div
+            key="answer"
+            initial={{ opacity: 0, height: 0 }}
+            animate={{ opacity: 1, height: "auto" }}
+            exit={{ opacity: 0, height: 0 }}
+            transition={{ duration: 0.3 }}
+            className="overflow-hidden"
+          >
+            <div className="w-[90%] h-[1px] bg-white/20 m-3 mx-auto"></div>
+            <p className="mt-2 text-white text-base">{answer}</p>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default FaqCard;
---
+++ src/components/navigation/NavigationMenu.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import { FaCircle } from "react-icons/fa"; //circle icon in nav bar
+import OrangeButton from "../buttons/OrangeButton";
+import Image from "next/image";
+
+const NavigationMenu = () => {
+  return (
+    <div>
+      {/* Navigation bar desktop */}
+      <nav className="hidden md:block  font-themeFont text-xs sticky z-50 top-0">
+        <div className="flex justify-between [&>*]:px-5 items-center w-screen h-auto border-white/20 shadow-xl backdrop-blur-md bg-[#08156B]/10">
+          {/* logo */}
+          <div className="aspect-auto h-10">
+            <a href="/">
+              <Image
+                src="https://www.quadrasecurity.com/logo_white.svg"
+                alt="logo"
+                className="w-full h-full object-cover"
+              />
+            </a>
+          </div>
+          {/* menu items */}
+          <div className="py-2">
+            <ul className="flex [&>li>svg]:text-[6px] [&>li>a]:p-1.5  items-center text-white gap-3">
+              <li className="transition-transform duration-300 ease-in-out hover:scale-110 ">
+                <a href="/" className="active:underline">
+                  Home
+                </a>
+              </li>
+              <li>
+                <FaCircle />
+              </li>
+              <li className="transition-transform duration-300 ease-in-out hover:scale-110">
+                <a href="/">Services</a>
+              </li>
+              <li>
+                <FaCircle />
+              </li>
+              <li className="transition-transform duration-300 ease-in-out hover:scale-110">
+                <a href="/">Blogs</a>
+              </li>
+              <li>
+                <FaCircle />
+              </li>
+              <li className="transition-transform duration-300 ease-in-out hover:scale-110">
+                <a href="/">About Us</a>
+              </li>
+              <li>
+                <FaCircle />
+              </li>
+              <li className="transition-transform duration-300 ease-in-out hover:scale-110">
+                <a href="/">Contact Us</a>
+              </li>
+              <li>
+                <FaCircle />
+              </li>
+              <li className="transition-transform duration-300 ease-in-out hover:scale-110">
+                <a href="/">Solutions</a>
+              </li>
+              <li>
+                {/* CTA(get your quotation button) */}
+                <div>
+                  <OrangeButton />
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <nav className="md:hidden">
+        <div className="bg-white">
+
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default NavigationMenu;
